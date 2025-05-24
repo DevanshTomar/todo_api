546833150d46692d7b73c95836509db92cef5b35
DevanshTomar <tomardevansh83@gmail.com>
2025-05-24 12:04:10 -0700

    feat: implement CRUD endpoints for todo items with input validation

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from database import Base
 
-class Todo(Base):
+class Todos(Base):
     """Table to store todos"""
     __tablename__ = "todos"
     id = Column(Integer, primary_key=True, index=True)
