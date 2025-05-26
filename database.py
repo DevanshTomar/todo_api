ecf8b6ec1e88d62689d3c52653c60f73c8efa3b0
DevanshTomar <tomardevansh83@gmail.com>
2025-05-26 12:05:05 -0700

    refactor: load database URL from environment variables using dotenv

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
+load_dotenv()
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5431/TodoApplicationDatabase"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
